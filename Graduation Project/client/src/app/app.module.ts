a4d6bc209c1f736104b445e761a3363d59dcb30a
abdallah <abdallah.shahene@gmail.com>
2022-02-22 22:07:40 +0200

    RefactoringArchtecture_abdallaH2

--- Graduation Project/client/src/app/app.module.ts
+++ Graduation Project/client/src/app/app.module.ts
@@ -12,12 +12,14 @@ import { MainLayoutComponent } from './components/main-layout/main-layout.compon
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { OrderComponent } from './components/order/order.component';
 import { SideBareComponent } from './components/side-bare/side-bare.component';
-import { UsersComponent } from './components/users/users.component';
+import { UsersComponent } from './components/loginAndRigister/users/users.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProductListComponent } from './components/Order/product-list/product-list.component';
 import { OrderMasterComponent } from './components/Order/OrderMaster/order-master/order-master.component';
 import { ProductDetailsComponent } from './components/Order/productDetails/product-details/product-details.component';
 import { ProdutDetailsComponent } from './components/Order/produt-details/produt-details.component';
+import { LoginComponent } from './components/loginAndRigister/login/login.component';
+import { RegisterComponent } from './components/loginAndRigister/register/register.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +36,8 @@ import { ProdutDetailsComponent } from './components/Order/produt-details/produt
     OrderMasterComponent,
     ProductDetailsComponent,
     ProdutDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
