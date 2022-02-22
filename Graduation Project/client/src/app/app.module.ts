fde8b3338ee86d005798ec409c2a3893bc7f1d0b
abdallah <abdallah.shahene@gmail.com>
2022-02-22 21:59:28 +0200

    RefactoringArchtecture_abdallaH

--- Graduation Project/client/src/app/app.module.ts
+++ Graduation Project/client/src/app/app.module.ts
@@ -7,18 +7,43 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { OrderComponent } from './components/order/order.component';
+import { SideBareComponent } from './components/side-bare/side-bare.component';
+import { UsersComponent } from './components/users/users.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductListComponent } from './components/Order/product-list/product-list.component';
+import { OrderMasterComponent } from './components/Order/OrderMaster/order-master/order-master.component';
+import { ProductDetailsComponent } from './components/Order/productDetails/product-details/product-details.component';
+import { ProdutDetailsComponent } from './components/Order/produt-details/produt-details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    MainLayoutComponent,
+    NotFoundComponent,
+    OrderComponent,
+    SideBareComponent,
+    UsersComponent,
+    HomeComponent,
+    DirectivesDirective,
+    ProductListComponent,
+    OrderMasterComponent,
+    ProductDetailsComponent,
+    ProdutDetailsComponent,
+  ],
   imports: [
-  BrowserModule,
+    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     //import core module which nav-bar lives in
-    CoreModule, 
+    CoreModule,
     //import shop module which products lives in
-    ShopModule
+    ShopModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ Graduation Project/client/src/app/components/Order/produt-details/produt-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produt-details',
+  templateUrl: './produt-details.component.html',
+  styleUrls: ['./produt-details.component.scss']
+})
+export class ProdutDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
