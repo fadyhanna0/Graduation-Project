9ed468a1e79472805f11eced46c529d88dfe45e8
abdallah <abdallah.shahene@gmail.com>
2022-02-23 13:52:08 +0200

    Update app-routing.module.ts

--- Graduation Project/client/src/app/app-routing.module.ts
+++ Graduation Project/client/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { pathToFileURL } from 'url';
+//import { pathToFileURL } from 'url';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/loginAndRigister/login/login.component';
-import { RegisterComponent } from './components/loginAndRigister/register/register.component';
-import { ProductListComponent } from './components/Order/product-list/product-list.component';
+// import { LoginComponent } from './components/loginAndRigister/login/login.component';
+// import { RegisterComponent } from './components/loginAndRigister/register/register.component';
+// import { ProductListComponent } from './components/Order/product-list/product-list.component';
 
 const routes: Routes = [
   { path: 'Home', component: HomeComponent },
-  { path: 'Login', component: LoginComponent },
+  // { path: 'Login', component: LoginComponent },
   // { path: 'Rigester', component: RegisterComponent },
   // { path: 'order', component: ProductListComponent },
 ];
